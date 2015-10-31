13b1219b43b94f8100d06c71b08cf5f68777e2c7
kunalvjti <kunal.pariani@nginx.com>
2015-10-30 18:01:28 -0700

    Updating status.sh and collecting much bigger JSON data set

--- status.sh
+++ status.sh
@@ -1,1 +1,1 @@
-while true; do curl demo.nginx.com/status | tee -a nginxplus_json_logs && echo "" >> nginxplus_json_logs; sleep 1; done
+for i in `seq 1 500`; do curl demo.nginx.com/status | tee -a nginxplus_json_logs && echo "" >> nginxplus_json_logs; sleep 5; done
